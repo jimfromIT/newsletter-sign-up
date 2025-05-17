68a6088e0918910b97567c49b945298cbc1ff4f8
jimfromIT <99919564+jimfromIT@users.noreply.github.com>
2025-05-17 18:19:28 +0200

    functions working
    New commit with working functions. Form validation and submit

---
+++ functions.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+/*
+function signUp(){
+    form.style.display = 'none';
+    success.classList.add("show"); 
+}
+
+function disMiss(){
+    success.classList.remove("show"); 
+    form.style.display = 'block';
+}
+    */
+
+const form = document.getElementById("signup");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email");
+    let isValid = true;
+
+    document.getElementById("email-error").textContent = "";
+
+    const emailRegex = /^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})$/;
+    if (!emailRegex.test(email.value)) {
+        document.getElementById("email-error").textContent = 
+            "Valid email required.";
+        email.classList.add("invalid");
+        isValid = false;
+    }
+    if (isValid) {
+        document.getElementById("signup").style.display = "none";
+        document.getElementById("successMsg").innerHTML = ` 
+        <h1>Thanks for subscribing!</h1> 
+        <p>A confirmation email has been sent to ${email.value}. Please open it and click the button inside to confirm your subscription. </p>
+        <button class="dismiss-btn" onclick="location.reload();" id="dismissBtn">Dismiss message</button>`;
+    }
+});
